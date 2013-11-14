fca1694085b67461ce7b90253b24b7a5920b3ee3
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2013-11-14 13:16:32 +0100

    Add substitution and adjoining operations

---
+++ src/NLP/LTAG/Tree.hs
@@ -0,0 +1,148 @@
+{-# LANGUAGE RecordWildCards #-}
+
+
+module NLP.LTAG.Tree
+(
+-- * Tree
+  Tree (..)
+-- * Path
+, Path
+, follow
+-- * Substitution
+, subst
+-- * Adjoining
+, AuxTree (..)
+, adjoin
+-- * Derivation
+, Deriv
+, Trans
+, derive
+) where
+
+
+import           Control.Applicative ((<$>))
+import           Control.Monad (foldM)
+
+
+-- | A tree with values of type 'a' kept in the interior nodes,
+-- and values of type 'b' kept in the leaf nodes.
+data Tree a b
+    = INode -- ^ Interior node
+        { labelI    :: a
+        , subTrees  :: [Tree a b] }
+    | FNode -- ^ Frontier node
+        { labelF    :: b }
+    deriving (Show, Eq, Ord)
+
+
+-- | Replace the tree on the given position.
+replaceChild :: Tree a b -> Int -> Tree a b -> Tree a b
+replaceChild t@INode{..} k t' = t { subTrees = replace subTrees k t' }
+replaceChild _ _ _ = error "replaceChild: frontier node"
+
+
+---------------------------------------------------------------------
+-- Path
+---------------------------------------------------------------------
+
+
+-- | A path can be used to extract a particular tree node.
+type Path = [Int]
+
+
+-- | Follow the path to a particular tree node. 
+follow :: Path -> Tree a b -> Maybe (Tree a b)
+follow = flip $ foldM step
+
+
+-- | Follow one step of the `Path`.
+step :: Tree a b -> Int -> Maybe (Tree a b)
+step (FNode _) _    = Nothing
+step (INode _ xs) k = xs !? k
+
+
+---------------------------------------------------------------------
+-- Substitution
+---------------------------------------------------------------------
+
+
+-- | Perform substitution on a tree. 
+subst
+    :: Path             -- ^ Place of the substitution
+    -> Tree a b         -- ^ Tree to be substituted
+    -> Tree a b         -- ^ Original tree
+    -> Maybe (Tree a b) -- ^ Resulting tree
+subst (k:ks) st t = do
+    replaceChild t k <$> (step t k >>= subst ks st)
+subst [] st _ = Just st
+
+
+---------------------------------------------------------------------
+-- Adjoining
+---------------------------------------------------------------------
+
+
+-- | An auxiliary tree.
+data AuxTree a b = AuxTree
+    { auxTree   :: Tree a b
+    , auxFoot   :: Path }
+    deriving (Show, Eq, Ord)
+
+
+-- | Perform adjoining operation on a tree.
+adjoin
+    :: Path             -- ^ Place of operation
+    -> AuxTree a b      -- ^ Tree to be adjoined
+    -> Tree a b         -- ^ Original tree
+    -> Maybe (Tree a b) -- ^ Resulting tree
+adjoin (k:ks) aux t = do
+    replaceChild t k <$> (step t k >>= adjoin ks aux)
+adjoin [] AuxTree{..} t = do
+    subst auxFoot t auxTree
+
+
+---------------------------------------------------------------------
+-- Derivation
+---------------------------------------------------------------------
+
+
+-- | A derivated tree is constructed by applying a sequence of transforming
+-- (substitution or adjoining) rules on particular positions of a tree.
+type Deriv a b = [(Path, Trans a b)]
+
+
+-- | Transformation of a tree.
+type Trans a b = Either (Tree a b) (AuxTree a b)
+
+
+-- | Derive a tree.
+derive :: Deriv a b -> Tree a b -> Maybe (Tree a b)
+derive =
+    flip $ foldM m
+  where
+    m t (pos, op) = case op of
+        Left x  -> subst  pos x t
+        Right x -> adjoin pos x t
+             
+
+---------------------------------------------------------------------
+-- Misc
+---------------------------------------------------------------------
+
+
+-- | Maybe a k-th element of a list.
+(!?) :: [a] -> Int -> Maybe a
+(x:xs) !? k
+    | k > 0     = xs !? (k-1)
+    | otherwise = Just x
+[] !? _ = Nothing
+
+
+-- | Replace the k-th element of a list.  If the given position is outside
+-- of the list domain, the returned list will be unchanged.  It the given
+-- index is negative, the first element will be replaced.
+replace :: [a] -> Int -> a -> [a]
+replace (x:xs) k y
+    | k > 0     = x : replace xs (k - 1) y
+    | otherwise = y : xs
+replace [] _ _  = []
--- src/NLP/LTAG/V1.hs
+++ src/NLP/LTAG/V1.hs
@@ -1,9 +1,20 @@
+{-# LANGUAGE RecordWildCards #-}
+
+
 -- | A first, simple version of LTAGs.
+-- Additional constraints over the adjoining operation are not
+-- taken into account.
 
 
 module NLP.LTAG.V1
-(
+( LTAG (..)
+, module NLP.LTAG.Tree
 ) where
+
+
+import           Data.Set (Set)
+import qualified Data.Set as S
+import           NLP.LTAG.Tree
 
 
 -- An LTAG grammar consists of:
@@ -21,11 +32,58 @@ module NLP.LTAG.V1
 --     from one special adjoining node called the foot node.  The foot
 --     node has the same label as the root node.
 --
--- Q: can we use the adjoining operation on non-terminal frontier nodes?
---
+-- Q: Can we use the adjoining operation on non-terminal frontier nodes?
+--    Or, to rephrase the question: can we perform the substitution
+--    operation with an auxiliary tree?  
+
+
+-- | We define LTAG with respect to the description above.  The sets of
+-- terminal and non-terminal symbols are implicit.
+data LTAG a b = LTAG
+    { startSym  :: a
+    , iniTrees  :: Set (E Tree a b)
+    , auxTrees  :: Set (E AuxTree a b) }
+    deriving (Show, Eq, Ord)
+
+
+-- | A grammar structure (e.g. tree). 
+-- TODO: Find appropriate name.
+type E t a b = t a (Either a b)
+
+
 -- In a lexicalized LTAG grammar, at least one terminal symbol (the anchor) 
 -- must appear at the frontier of all (initial or auxiliary) trees.
---
+-- It would be hard to ensure this property on the level of the type system.
+
+
+-- -- | A derivated tree is constructed by applying a sequence of transforming
+-- -- (substitution or adjoining) rules on particular positions of a tree.
+-- type Deriv a b = [(Path, Trans a b)]
+-- 
+-- 
+-- -- | A transformation of a tree.
+-- type Trans a b = Either (E Tree a b) (E AuxTree a b)
+-- 
+-- 
+-- -- | Derive a tree.
+-- derive :: Deriv a b -> E Tree a b -> Maybe (E Tree a b)
+
+
+-- -- | Perform the given transformation.
+-- perform :: Trans a b -> IniTree a b -> Maybe (IniTree a b)
+-- perform Subst{..} t = do
+--     
+-- 
+-- 
+-- -- | Enumerate trees which can be generated from the tree by using the
+-- -- substitution rules.
+-- subst :: LTAG a b -> 
+
+
+-- | Generate the tree-language represented by the grammar.
+
+
+
 -- IMPORTANT: note the difference between the two:
 -- * Perform lexical insertion -> substitute terminal symbols with lexical items,
 -- * Lexicalized grammar -> each tree has at least on terminal frontier node.
