6828b043373e6d6d5ea18fd842eb46a1b04fb814
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2015-06-30 20:28:30 +0200

    Current snapshot

--- src/NLP/LTAG/FlatFS.hs
+++ src/NLP/LTAG/FlatFS.hs
@@ -15,115 +15,115 @@ import qualified Data.Partition             as R
 import qualified Pipes                      as P
 
 
--- | A feature structure.
-type FS x y v = M.Map x (Either y v)
-
-
--- | Environment stores information about variable values and
--- relations between them.
-data Env y v = Env {
-    -- | The partition represents an equality relation between
-    -- variables.
-      part :: Partition v
-    -- | Valuation of variables.
-    , eval :: M.Map v y
-    } deriving (Show, Eq, Ord)
-
-
--- -- | A unification monad contains information about a feature
--- -- structure and a corresponding environment.
--- data UniSt y v = UniSt
+-- -- | A feature structure.
+-- type FS x y v = M.Map x (Either y v)
 -- 
 -- 
--- -- | Unification transformer.
--- type UniT y v m = E.StateT (UniSt y v) m
+-- -- | Environment stores information about variable values and
+-- -- relations between them.
+-- data Env y v = Env {
+--     -- | The partition represents an equality relation between
+--     -- variables.
+--       part :: Partition v
+--     -- | Valuation of variables.
+--     , eval :: M.Map v y
+--     } deriving (Show, Eq, Ord)
 -- 
 -- 
--- -- | Run unification computation.
--- runUniT
---     :: FS x y v     -- ^ Feature structure
---     -> Env y v      -- ^ And a corresponding environment
---     -> UniT y v m a -- ^ Unification computation
---     -> m (Maybe (FS x y v, Env y v))
--- runUniT = undefined
+-- -- -- | A unification monad contains information about a feature
+-- -- -- structure and a corresponding environment.
+-- -- data UniSt y v = UniSt
+-- -- 
+-- -- 
+-- -- -- | Unification transformer.
+-- -- type UniT y v m = E.StateT (UniSt y v) m
+-- -- 
+-- -- 
+-- -- -- | Run unification computation.
+-- -- runUniT
+-- --     :: FS x y v     -- ^ Feature structure
+-- --     -> Env y v      -- ^ And a corresponding environment
+-- --     -> UniT y v m a -- ^ Unification computation
+-- --     -> m (Maybe (FS x y v, Env y v))
+-- -- runUniT = undefined
+-- -- 
+-- -- 
+-- -- -- | Assign value to a given variable.
+-- -- assign :: Monad m => y -> v -> EnvT y v m ()
+-- -- assign = undefined
+-- 
+-- 
+-- 
+-- -- | Envoironment transformer.
+-- type EnvT y v m = E.StateT (Env y v) m
+-- 
+-- 
+-- -- | Envoironment monad.
+-- type EnvM y v = EnvT y v Identity
+-- 
+-- 
+-- -- | Run computation within the environment monad.
+-- runEnvM :: Env y v -> EnvM y v a -> Maybe (Env y v, a)
+-- runEnvM = undefined
+-- 
+-- 
+-- -- | Run computation within the environment transformer.
+-- runEnvT :: Monad m => Env y v -> EnvT y v m a -> m (Maybe (Env y v, a))
+-- runEnvT = undefined
 -- 
 -- 
 -- -- | Assign value to a given variable.
 -- assign :: Monad m => y -> v -> EnvT y v m ()
 -- assign = undefined
-
-
-
--- | Envoironment transformer.
-type EnvT y v m = E.StateT (Env y v) m
-
-
--- | Envoironment monad.
-type EnvM y v = EnvT y v Identity
-
-
--- | Run computation within the environment monad.
-runEnvM :: Env y v -> EnvM y v a -> Maybe (Env y v, a)
-runEnvM = undefined
-
-
--- | Run computation within the environment transformer.
-runEnvT :: Monad m => Env y v -> EnvT y v m a -> m (Maybe (Env y v, a))
-runEnvT = undefined
-
-
--- | Assign value to a given variable.
-assign :: Monad m => y -> v -> EnvT y v m ()
-assign = undefined
-
-
--- | Mark that the two varialbes are equal.
-egal :: Monad m => v -> v -> EnvT y v m ()
-egal = undefined
-
-
--- | Regular unification with no enviroment sharing.  The
--- resulting environment will be the extension of the first
--- environment given as an argument.
-unify
-    :: (Ord x, Ord y, Ord v)
-    => FS x y v     -- ^ The first FS
-    -> Env y v      -- ^ And the corresponding environment
-    -> FS x y v     -- ^ The second FS
-    -> Env y v      -- ^ And the corresponding environment
-    -> Maybe (FS x y b, Env y v)
-unify fs1 env1 fs2 env2 =
-    runEnvM env2 $ do
-    runEnvT env1 $ do
-    P.runListT $ do
-        (attr, vals) <- each fs0
-        case S.toList vals of 
-            [val] -> return (attr, val)
-            [Left x, Left y] -> do 
-                liftEnv1 $ guard $ x == y
-                return (attr, Left x)
-            [Left x, Right v] -> do
-                liftEnv2 $ assign x v
-
-            _ -> error "unify: sth.'s wrong"
-  where
-    liftEnv1 = lift
-    liftEnv2 = lift . lift
-    fs0 = M.toList $ M.unionWith S.union
-        (S.singleton <$> fs1)
-        (S.singleton <$> fs2)
-
-
---         -- go over through all possible attributes
---         x <- each $ S.toList $ S.union
---             (M.keysSet fs1) (M.keysSet fs2)
-
-
---------------------------------------------------
--- UTILS
---------------------------------------------------
-
-
--- | ListT from a list.
-each :: Monad m => [a] -> P.ListT m a
-each = P.Select . P.each
+-- 
+-- 
+-- -- | Mark that the two varialbes are equal.
+-- egal :: Monad m => v -> v -> EnvT y v m ()
+-- egal = undefined
+-- 
+-- 
+-- -- | Regular unification with no enviroment sharing.  The
+-- -- resulting environment will be the extension of the first
+-- -- environment given as an argument.
+-- unify
+--     :: (Ord x, Ord y, Ord v)
+--     => FS x y v     -- ^ The first FS
+--     -> Env y v      -- ^ And the corresponding environment
+--     -> FS x y v     -- ^ The second FS
+--     -> Env y v      -- ^ And the corresponding environment
+--     -> Maybe (FS x y b, Env y v)
+-- unify fs1 env1 fs2 env2 =
+--     runEnvM env2 $ do
+--     runEnvT env1 $ do
+--     P.runListT $ do
+--         (attr, vals) <- each fs0
+--         case S.toList vals of 
+--             [val] -> return (attr, val)
+--             [Left x, Left y] -> do 
+--                 liftEnv1 $ guard $ x == y
+--                 return (attr, Left x)
+--             [Left x, Right v] -> do
+--                 liftEnv2 $ assign x v
+-- 
+--             _ -> error "unify: sth.'s wrong"
+--   where
+--     liftEnv1 = lift
+--     liftEnv2 = lift . lift
+--     fs0 = M.toList $ M.unionWith S.union
+--         (S.singleton <$> fs1)
+--         (S.singleton <$> fs2)
+-- 
+-- 
+-- --         -- go over through all possible attributes
+-- --         x <- each $ S.toList $ S.union
+-- --             (M.keysSet fs1) (M.keysSet fs2)
+-- 
+-- 
+-- --------------------------------------------------
+-- -- UTILS
+-- --------------------------------------------------
+-- 
+-- 
+-- -- | ListT from a list.
+-- each :: Monad m => [a] -> P.ListT m a
+-- each = P.Select . P.each
---
+++ src/NLP/LTAG/Tree2.hs
@@ -0,0 +1,195 @@
+{-# LANGUAGE RecordWildCards #-}
+
+
+module NLP.LTAG.Tree2 where
+
+
+import           Control.Applicative ((<$>))
+import           Control.Arrow (first)
+import           Control.Monad (foldM)
+
+
+-- | A tree with values of type 'n' kept in the interior nodes,
+-- values of type 'b' kept in the leaf nodes, FS attributes of
+-- type 'a', FS values of type 'v' and FS variables of type 'r'.
+data Tree n l a v r
+    = INode -- ^ Interior node
+        { labelI    :: n
+        , topFS     :: FS a v r
+        -- TODO: empty for substitution nodes?
+        , botFS     :: FS a v r
+        , subTrees  :: [Tree n l a v r] }
+    | FNode -- ^ Foot node
+        { labelI    :: n
+        , topFS     :: FS a v r
+        , botFS     :: FS a v r }
+    | LNode -- ^ Leaf node
+        { labelL    :: l
+        , topFS     :: FS a v r
+        -- TODO: empty for leaf nodes?
+        , botFS     :: FS a v r }
+    deriving (Show, Eq, Ord)
+
+
+-- | List of frontier values. 
+toWord :: Tree n l a v r -> [l]
+toWord t = case t of
+    INode{..}   -> concatMap toWord subTrees
+    LNode{..}   -> [labelL]
+    FNode{..}   -> []
+
+
+-- | Transform a tree into an equivalent tree with dummy attributes
+-- so that the relations between variables in the resultant tree are
+-- of a height-distance at most 1.
+dummify :: Tree n l a v r -> Tree n l (Either a Int) v r
+dummify 
+
+
+-- -- | Show a tree given the showing functions for label values.
+-- showTree :: (a -> String) -> (b -> String) -> Tree a b -> String
+-- showTree f g = unlines . go
+--   where
+--     go t = case t of
+--         INode{..}   -> ("INode " ++ f labelI)
+--             : map ("  " ++) (concatMap go subTrees)
+--         FNode{..}   -> ["FNode " ++ g labelF]
+-- 
+-- 
+-- -- | Like `showTree`, but using the default `Show` instances
+-- -- to present label values.
+-- showTree' :: (Show a, Show b) => Tree a b -> String
+-- showTree' = showTree show show
+-- 
+-- 
+-- -- | Replace the tree on the given position.
+-- replaceChild :: Tree a b -> Int -> Tree a b -> Tree a b
+-- replaceChild t@INode{..} k t' = t { subTrees = replace subTrees k t' }
+-- replaceChild _ _ _ = error "replaceChild: frontier node"
+-- 
+-- 
+-- ---------------------------------------------------------------------
+-- -- Path
+-- ---------------------------------------------------------------------
+-- 
+-- 
+-- -- | A path can be used to extract a particular tree node.
+-- type Path = [Int]
+-- 
+-- 
+-- -- | Follow the path to a particular tree node. 
+-- follow :: Path -> Tree a b -> Maybe (Tree a b)
+-- follow = flip $ foldM step
+-- 
+-- 
+-- -- | Follow one step of the `Path`.
+-- step :: Tree a b -> Int -> Maybe (Tree a b)
+-- step (FNode _) _    = Nothing
+-- step (INode _ xs) k = xs !? k
+-- 
+-- 
+-- ---------------------------------------------------------------------
+-- -- Substitution
+-- ---------------------------------------------------------------------
+-- 
+-- 
+-- -- | Perform substitution on a tree. 
+-- subst
+--     :: Path             -- ^ Place of the substitution
+--     -> Tree a b         -- ^ Tree to be substituted
+--     -> Tree a b         -- ^ Original tree
+--     -> Maybe (Tree a b) -- ^ Resulting tree
+-- subst (k:ks) st t = do
+--     replaceChild t k <$> (step t k >>= subst ks st)
+-- subst [] st _ = Just st
+-- 
+-- 
+-- ---------------------------------------------------------------------
+-- -- Adjoining
+-- ---------------------------------------------------------------------
+-- 
+-- 
+-- -- | An auxiliary tree.
+-- data AuxTree a b = AuxTree
+--     { auxTree   :: Tree a b
+--     , auxFoot   :: Path }
+--     deriving (Show, Eq, Ord)
+-- 
+-- 
+-- -- | Perform adjoining operation on a tree.
+-- adjoin
+--     :: Path             -- ^ Where to adjoin
+--     -> AuxTree a b      -- ^ Tree to be adjoined
+--     -> Tree a b         -- ^ Tree with the node to be modified
+--     -> Maybe (Tree a b) -- ^ Resulting tree
+-- adjoin (k:ks) aux t = do
+--     replaceChild t k <$> (step t k >>= adjoin ks aux)
+-- adjoin [] AuxTree{..} t = do
+--     subst auxFoot t auxTree
+-- 
+-- 
+-- ---------------------------------------------------------------------
+-- -- Derivation
+-- ---------------------------------------------------------------------
+-- 
+-- 
+-- -- | A derived tree is constructed by applying a sequence of
+-- -- transforming (substitution or adjoining) rules on particular
+-- -- positions of a tree.  The `Deriv` sequence represents a
+-- -- derivation process.  One could also construct a derivation
+-- -- tree, which to some extent abstracts over the particular order
+-- -- of derivations (when it doesn't matter).
+-- type Deriv a b = [(Path, Trans a b)]
+-- 
+-- 
+-- -- | Transformation of a tree.
+-- type Trans a b = Either (Tree a b) (AuxTree a b)
+-- 
+-- 
+-- -- | Derive a tree.
+-- derive :: Deriv a b -> Tree a b -> Maybe (Tree a b)
+-- derive =
+--     flip $ foldM m
+--   where
+--     m t (pos, op) = case op of
+--         Left x  -> subst  pos x t
+--         Right x -> adjoin pos x t
+-- 
+-- 
+-- ---------------------------------------------------------------------
+-- -- Traversal
+-- ---------------------------------------------------------------------
+-- 
+-- 
+-- -- | Return all tree paths with corresponding subtrees.
+-- walk :: Tree a b -> [(Path, Tree a b)]
+-- walk =
+--     map (first reverse) . go []
+--   where
+--     go acc n@INode{..} = (acc, n) : concat
+--         [ go (k:acc) t
+--         | (k, t) <- zip [0..] subTrees ]
+--     go acc n@FNode{..} = [(acc, n)]
+-- 
+-- 
+-- ---------------------------------------------------------------------
+-- -- Misc
+-- ---------------------------------------------------------------------
+-- 
+-- 
+-- -- | Maybe a k-th element of a list.
+-- (!?) :: [a] -> Int -> Maybe a
+-- (x:xs) !? k
+--     | k > 0     = xs !? (k-1)
+--     | otherwise = Just x
+-- [] !? _ = Nothing
+-- 
+-- 
+-- -- | Replace the k-th element of a list.  If the given position is outside
+-- -- of the list domain, the returned list will be unchanged.  It the given
+-- -- index is negative, the first element will be replaced.
+-- replace :: [a] -> Int -> a -> [a]
+-- replace (x:xs) k y
+--     | k > 0     = x : replace xs (k - 1) y
+--     | otherwise = y : xs
+-- replace [] _ _  = []
