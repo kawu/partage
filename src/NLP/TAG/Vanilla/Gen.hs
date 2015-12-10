67189e75ef29600b78d90318fbecd2e26fe60b50
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2015-12-10 23:50:50 +0100

    Finish the preliminary version of generation module

--- src/NLP/TAG/Vanilla/Gen.hs
+++ src/NLP/TAG/Vanilla/Gen.hs
@@ -1,21 +1,22 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 
 -- | An simple, experimental tree generation module.
 
 
 module NLP.TAG.Vanilla.Gen
-( Gen
-, generate
-, newGenST
+( generate
 ) where
 
 
 
 -- import           Control.Applicative ((<$>))
 -- import           Control.Monad (void, when, forM_)
-import           Control.Monad (when)
+-- import           Control.Monad (when)
 import qualified Control.Monad.State.Strict   as E
 -- import           Control.Monad.Trans.Maybe (MaybeT (..))
 -- import           Control.Monad.Trans.Class (lift)
@@ -26,8 +27,9 @@ import           Data.Maybe (maybeToList)
 import qualified Data.Set as S
 import qualified Data.PSQueue as Q
 import           Data.PSQueue (Binding(..))
+import qualified Data.Tree as R
 
-import           NLP.TAG.Vanilla.Tree
+import           NLP.TAG.Vanilla.Tree.Other hiding (SomeTree)
 
 
 --------------------------
@@ -36,7 +38,11 @@ import           NLP.TAG.Vanilla.Tree
 
 
 -- | Some TAG tree.
-type SomeTree n t = Either (Tree n t) (AuxTree n t)
+-- type SomeTree n t = Either (Tree n t) (AuxTree n t)
+type SomeTree n t = Tree n t
+
+
+deriving instance (Ord n, Ord t) => (Ord (SomeTree n t))
 
 
 -- | A TAG grammar.
@@ -50,31 +56,32 @@ type Gram n t = S.Set (SomeTree n t)
 
 -- | Size of a tree, i.e. number of nodes.
 treeSize :: SomeTree n t -> Int
-treeSize st =
-    case st of
-        Left t -> realSize t
-        Right (AuxTree t _) -> realSize t
-  where
-    realSize INode{..} = 1 + sum (map realSize subTrees)
-    realSize FNode{..} = 1
+treeSize = length . R.flatten
+--     case st of
+--         Left t -> realSize t
+--         Right (AuxTree t _) -> realSize t
+--   where
+--     realSize INode{..} = 1 + sum (map realSize subTrees)
+--     realSize FNode{..} = 1
 
 
--- | Is it a final tree (i.e. does it contain only terminals in its leaves?)
-final :: SomeTree n t -> Bool
-final st =
-    case st of
-        Left t  -> doit t
-        Right _ -> False
-  where
-    doit INode{..} = (not.null) subTrees
-                  && and (map doit subTrees)
-    doit FNode{..} = True
+-- -- | Is it a final tree (i.e. does it contain only terminals
+-- in its leaves?)
+-- final :: SomeTree n t -> Bool
+-- final st =
+--     case st of
+--         Left t  -> doit t
+--         Right _ -> False
+--   where
+--     doit INode{..} = (not.null) subTrees
+--                   && and (map doit subTrees)
+--     doit FNode{..} = True
 
 
--- | Extract the underlying tree.
-theTree :: SomeTree n t -> Tree n t
-theTree (Left t) = t
-theTree (Right (AuxTree t _)) = t
+-- -- | Extract the underlying tree.
+-- theTree :: SomeTree n t -> Tree n t
+-- theTree (Left t) = t
+-- theTree (Right (AuxTree t _)) = t
 
 
 --------------------------
@@ -84,7 +91,7 @@ theTree (Right (AuxTree t _)) = t
 
 -- | Underlying state of the generation pipe.
 data GenST n t = GenST {
-      waiting :: Q.PSQ Int (SomeTree n t)
+      waiting :: Q.PSQ (SomeTree n t) Int
     -- ^ Queue of the derived trees yet to be visited.
     , doneSet :: S.Set (SomeTree n t)
     }
@@ -94,7 +101,7 @@ data GenST n t = GenST {
 newGenST :: (Ord n, Ord t) => Gram n t -> GenST n t
 newGenST gramSet = GenST {
       waiting = Q.fromList
-        [ treeSize t :-> t
+        [ t :-> treeSize t
         | t <- S.toList gramSet ]
     , doneSet = S.empty }
 
@@ -102,11 +109,13 @@ newGenST gramSet = GenST {
 -- | Pop the tree with the lowest score from the queue.
 pop
     :: (E.MonadState (GenST n t) m, Ord n, Ord t)
+    -- => m (Maybe (SomeTree n t))
     => ListT m (SomeTree n t)
 pop = do
     mayTree <- E.state $ \s@GenST{..} -> case Q.minView waiting of
         Nothing -> (Nothing, s)
-        Just (_ :-> t, q) -> (Just t, s {waiting=q})
+        Just (t :-> _, q) -> (Just t, s {waiting=q})
+    -- return mayTree
     some $ maybeToList mayTree
 
 
@@ -114,7 +123,13 @@ pop = do
 push :: (E.MonadState (GenST n t) m, Ord n, Ord t) => SomeTree n t -> m ()
 push t = do
     E.modify $ \s -> s
-        {waiting = Q.insert (treeSize t) t (waiting s)}
+        {waiting = Q.insert t (treeSize t) (waiting s)}
+
+
+-- | Save tree as visited.
+save :: (E.MonadState (GenST n t) m, Ord n, Ord t) => SomeTree n t -> m ()
+save t = do
+    E.modify $ \s -> s {doneSet = S.insert t (doneSet s)}
 
 
 -- | Check if tree already visited.
@@ -132,7 +147,16 @@ visited t = do
 
 
 -- | Type of the generator.
-type Gen m n t = Producer (Tree n t) (E.StateT (GenST n t) m) ()
+-- type Gen m n t = Producer (Tree n t) (E.StateT (GenST n t) m) ()
+type Gen m n t = E.StateT (GenST n t) (Producer (Tree n t) m) ()
+
+
+-- | Run generation on the given grammar.
+-- Generate trees up to a given size.
+generate
+    :: (Monad m, Ord n, Ord t)
+    => Gram n t -> Int -> Producer (Tree n t) m ()
+generate gram k = E.evalStateT (genPipe k) (newGenST gram)
 
 
 --------------------------
@@ -140,25 +164,36 @@ type Gen m n t = Producer (Tree n t) (E.StateT (GenST n t) m) ()
 --------------------------
 
 
--- | A function which generates non-auxiliary trees derived from the grammar.
-generate :: (Monad m, Ord n, Ord t) => Gen m n t
-generate = runListT $ do
+-- | A function which generates trees derived from the grammar.
+genPipe :: (Monad m, Ord n, Ord t) => Int -> Gen m n t
+genPipe k = runListT $ do
     -- pop best-score tree from the queue
     t <- pop
+    E.guard $ treeSize t <= k
+    lift $ do
+        genStep t
+        genPipe k
 
+
+-- | Generation step.
+genStep :: (Monad m, Ord n, Ord t) => SomeTree n t -> Gen m n t
+genStep t = runListT $ do
     -- check if it's not in the set of visited trees yet
     E.guard . not =<< visited t
 
-    -- if final, yield it
-    when (final t) $
-        lift . yield $ theTree t
-
-    -- take some visited tree 'u'
+    -- we have to extract the set of visited states before
+    -- we save the new one
     treeSet <- E.gets doneSet
-    u <- some $ S.toList treeSet
 
-    -- find all possible combinations of 't' and 'u' and add them
-    -- to the waiting queue.
+    -- save tree `t` and yield it
+    save t
+    lift . lift $ yield t
+
+    -- find all possible combinations of 't' and some visited 'u',
+    -- and add them to the waiting queue.  NOTE: at this point we
+    -- know that `v` cannot yet be visited; it must be larger than
+    -- any tree in the set of visited trees.
+    u <- some $ S.toList treeSet
     v <- combinations t u
     push v
 
@@ -170,11 +205,11 @@ generate = runListT $ do
 
 -- | Return all possible combinations of the two trees.
 combinations
-    :: Monad m
+    :: (Monad m, Eq n, Eq t)
     => SomeTree n t
     -> SomeTree n t
     -> ListT m (SomeTree n t)
-combinations = undefined
+combinations s t = some $ inject s t ++ inject t s
 
 
 --------------------------
