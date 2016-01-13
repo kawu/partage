96ea1b752918191c4d69d46237cb17063a6aa877
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2016-01-13 01:12:04 +0100

    Update path to the parser module in tests

--- tests/Parser.hs
+++ tests/Parser.hs
@@ -15,7 +15,7 @@ import qualified Data.Set as S
 
 import qualified NLP.TAG.Vanilla.Rule as R
 import qualified NLP.TAG.Vanilla.Auto.DAWG  as A
-import qualified NLP.TAG.Vanilla.Earley.AutoAP as E
+import qualified NLP.TAG.Vanilla.Earley as E
 import qualified NLP.TAG.Vanilla.Tree as E
 
 import qualified TestSet as T
